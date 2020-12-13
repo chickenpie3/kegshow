b062dc9fdb937db9d0849240fa405f3634af9e98
David Meunier <david.meunier@mavtek.com>
2020-12-13 13:34:13 -0500

    Use Cognito Identity Pool instead of hardcoded credentials

--- web/static/monitor.js
+++ web/static/monitor.js
@@ -37,49 +37,55 @@ function generateID(length) {
 
 function monitor(topics, onmessage) {
 
-	if (!mqttClient) {
-		awsIot = require('aws-iot-device-sdk');
-	    mqttClient = awsIot.device({
-			//
-			// Set the AWS region we will operate in.
-			//
-			region: 'us-east-1',
-			//
-			////Set the AWS IoT Host Endpoint
-			host:'a3tkzsco97gkug-ats.iot.us-east-1.amazonaws.com',
-			//
-			// Use the clientId created earlier.
-			//
-			clientId: generateID(32),
-			//
-			// Connect via secure WebSocket
-			//
-			protocol: 'wss',
-			//
-			// Set the maximum reconnect time to 8 seconds; this is a browser application
-			// so we don't want to leave the user waiting too long for reconnection after
-			// re-connecting to the network/re-opening their laptop/etc...
-			//
-			maximumReconnectTimeMs: 8000,
-			//
-			// Enable console debugging information (optional)
-			//
-			debug: true,
-			//
-			// IMPORTANT: the AWS access key ID, secret key, and sesion token must be
-			// initialized with empty strings.
-			//
-			accessKeyId: 'AKIAIBAKEMQ27O34LIUQ',
-			secretKey: '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'
-		});
-	}
-
 	messageCallback = onmessage;
 	mqttTopics = topics;
 
-	mqttClient.on('connect', mqttClientConnectHandler);
-    mqttClient.on('message', mqttClientMessageHandler);
+	if (!mqttClient) {
+        // Initialize the Amazon Cognito credentials provider
+        AWS.config.region = 'us-east-1';
+        AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+            IdentityPoolId: 'us-east-1:c448a2a8-f739-4915-a594-d9fb157541fd'
+        });
 
+        AWS.config.credentials.get(function(){
+
+            // Credentials will be available when this function is called.
+
+            awsIot = require('aws-iot-device-sdk');
+            mqttClient = awsIot.device({
+                //
+                // Set the AWS region we will operate in.
+                //
+                region: 'us-east-1',
+                //
+                ////Set the AWS IoT Host Endpoint
+                host:'a3tkzsco97gkug-ats.iot.us-east-1.amazonaws.com',
+                //
+                // Use the clientId created earlier.
+                //
+                clientId: generateID(32),
+                //
+                // Connect via secure WebSocket
+                //
+                protocol: 'wss',
+                //
+                // Set the maximum reconnect time to 8 seconds; this is a browser application
+                // so we don't want to leave the user waiting too long for reconnection after
+                // re-connecting to the network/re-opening their laptop/etc...
+                //
+                maximumReconnectTimeMs: 8000,
+                //
+                // Enable console debugging information (optional)
+                //
+                debug: true,
+                accessKeyId: AWS.config.credentials.accessKeyId,
+                secretKey: AWS.config.credentials.secretAccessKey,
+                sessionToken: AWS.config.credentials.sessionToken
+            });
+            mqttClient.on('connect', mqttClientConnectHandler);
+            mqttClient.on('message', mqttClientMessageHandler);
+        });
+    }
 }
 
 module.exports = {
